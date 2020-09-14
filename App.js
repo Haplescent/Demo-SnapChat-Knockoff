cb215abf2bb0ec07757fe1899f5b9ebf96bb9677
John Merritt <johnmerritt95@gmail.com>
2020-09-13 20:01:08 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Container, Content } from 'native-base'
+import Swiper from 'react-native-swiper'
 
+import Camera from './Components/Camera'
 const styles = StyleSheet.create({
-  container: {
+  slideDefault: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
   },
-});
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
+export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      outerScrollEnabled: true
+    }
+  }
+
+  verticalScroll = (index) => {
+    if (index !== 1) {
+      this.setState({
+        outerScrollEnabled: false
+      })
+    }
+    else {
+      this.setState({
+        outerScrollEnabled: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Swiper
+            loop={false}
+            showsPagination={false}
+            index={1}
+            scrollEnabled={this.state.outerScrollEnabled}
+          >
+
+            <View style={styles.slideDefault}>
+              <Text style={styles.text}>Chat</Text>
+            </View>
+            <Swiper
+              loop={false}
+              showsPagination={false}
+              horizontal={false}
+              index={1}
+              onIndexChanged={(index) => this.verticalScroll(index)}
+            >
+              <View style={styles.slideDefault}>
+                <Text style={styles.text}>Search</Text>
+              </View>
+              <View style={{ flex: 1 }}>
+                <Camera></Camera>
+              </View>
+              <View style={styles.slideDefault}>
+                <Text style={styles.text}>Memories</Text>
+              </View>
+            </Swiper>
+            <View style={styles.slideDefault}>
+              <Text style={styles.text}>Stories</Text>
+            </View>
+          </Swiper>
+        </Content>
+      </Container>
+    );
+  }
+}
